3c9ec791381c2edb7f14e31d772358e92e968b09
alisq <ali.shamas.qadeer@gmail.com>
2021-10-27 09:39:15 -0400

    added cookie for preamble.

--- js/script.js
+++ js/script.js
@@ -32,11 +32,22 @@ let pages = [];
   .then(response => response.json())
     .then(j => {
       
-      console.log(j)
+      // console.log(j)
       
 
+
+  
+
+//CHECK FOR COOKIE FOR PREAMBLE.
+ if (getCookie("preambleViewed") !=  "true") {  
+      setCookie("preambleViewed","true",7);
       $(".preamble--text").html(j[1].body)
-      $(".preamble--cover").fadeIn(200)
+      $(".preamble--cover, .preamble--text").fadeIn(200)
+}
+
+  //  document.cookie = "preambleViewed=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  // console.log("it's there: "+getCookie("preambleViewed"));
+
   pages = [...j]
   //console.log(pages)
 
@@ -385,3 +396,27 @@ function shuffle(array) {
 //   $("#make-book").click()
   
 // },1000);
+
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
